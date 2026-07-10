2d1c5cad1fbacd472a3e5aa810809387507e16c9
Sayan431 <sayanchatterjee1@virtualemployee.com>
2026-07-10 10:22:59 +0530

    fix: use correct env var names for cloudinary config

--- backend/cloudinary_config.py
+++ backend/cloudinary_config.py
@@ -2,8 +2,8 @@ import cloudinary
 import os
 
 cloudinary.config(
-    cloud_name=os.getenv("nahlb563"),
-    api_key=os.getenv("692771129451256"),
-    api_secret=os.getenv("zBB50QxyYtxmVFZZUpy_17du-DM"),
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True,
 )
